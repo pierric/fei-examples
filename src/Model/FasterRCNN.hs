dff186f67c7b1f3fec36a4a7cab51ddc6ff0b76b
Jiasen Wu <jiasenwu@hotmail.com>
2019-07-04 17:48:28 +0200

    minor

--- src/Model/FasterRCNN.hs
+++ src/Model/FasterRCNN.hs
@@ -16,14 +16,14 @@ symbolTrain anchor_scales anchor_ratios rpn_feature_stride rpn_batch_rois rpn_pr
     rpnBoxWeight <- variable "bbox_weight"
 
     convFeat <- VGG.getFeature dat [2, 2, 3, 3, 3] [64, 128, 256, 512, 512] False
-    
+
     rpnConv <- convolution "rpn_conv_3x3" (#data := convFeat .& #kernel := [3,3] .& #pad := [1,1] .& #num_filter := 512 .& Nil)
     rpnRelu <- activation "rpn_relu" (#data := rpnConv .& #act_type := #relu .& Nil)
 
     rpnClsScore <- convolution "rpn_cls_score" (#data := rpnRelu .& #kernel := [1,1] .& #pad := [0,0] .& #num_filter := 2 * numAnchors .& Nil)
     rpnClsScoreReshape <- reshape "rpn_cls_score_reshape" (#data := rpnClsScore .& #shape := [0, 2, -1, 0] .& Nil)
-    rpnClsProb <- softmaxoutput "rpn_cls_prob" (#data := rpnClsScoreReshape .& #label := rpnLabel .& #multi_output := True .&
-                                               #normalization := #valid .& #use_ignore := True .& #ignore_label := -1 .& Nil)
+    rpnClsProb <- softmaxoutput "rpn_cls_prob" (#data := rpnClsScoreReshape .& #label := rpnLabel .& #multi_output := True
+                                             .& #normalization := #valid .& #use_ignore := True .& #ignore_label := -1 .& Nil)
     rpnClsAct <- softmax "rpn_cls_act" (#data := rpnClsScoreReshape .& #axis := 1 .& Nil)
     rpnClsActReshape <- reshape "rpn_cls_act_reshape" (#data := rpnClsAct .& #shape := [0, 2 * numAnchors, -1, 0] .& Nil)
 
@@ -34,11 +34,10 @@ symbolTrain anchor_scales anchor_ratios rpn_feature_stride rpn_batch_rois rpn_pr
 
     rpnBBoxLoss <- _MakeLoss "rpn_bbox_loss" (#data := rpnBBoxLoss_ .& #grad_scale := 1.0 / fromIntegral rpn_batch_rois .& Nil)
 
-    rois <- _contrib_MultiProposal "rois" (
-        #cls_prob := rpnClsActReshape .& #bbox_pred := rpnBBoxPred .& #im_info := imInfo .& 
-        #feature_stride := rpn_feature_stride .& #scales := anchor_scales .& #ratios := anchor_ratios .&
-        #rpn_pre_nms_top_n := rpn_pre_topk .& #rpn_post_nms_top_n := rpn_post_topk .& 
-        #threshold := rpn_nms_thresh .& #rpn_min_size := rpn_min_size .& Nil)
+    rois <- _contrib_MultiProposal "rois" (#cls_prob := rpnClsActReshape .& #bbox_pred := rpnBBoxPred .& #im_info := imInfo
+                                        .& #feature_stride := rpn_feature_stride .& #scales := anchor_scales .& #ratios := anchor_ratios
+                                        .& #rpn_pre_nms_top_n := rpn_pre_topk .& #rpn_post_nms_top_n := rpn_post_topk
+                                        .& #threshold := rpn_nms_thresh .& #rpn_min_size := rpn_min_size .& Nil)
 
     return $ Symbol rois
 
