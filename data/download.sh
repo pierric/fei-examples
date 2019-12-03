df7f92bf6eb74d5b54fc0ffa6faf671840aad154
Jiasen Wu <jiasenwu@hotmail.com>
2019-12-03 16:45:21 +0100

    update download script (cifar10 v2 record file)

--- data/download.sh
+++ data/download.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
 # CIFAR-10 test
-curl http://data.dmlc.ml/data/cifar10/cifar10_train.rec -o cifar10_train.rec
-curl http://data.dmlc.ml/data/cifar10/cifar10_val.rec -o cifar10_val.rec
+curl http://data.mxnet.io/data/cifar10/cifar10_train.rec -o cifar10_train.rec
+curl http://data.mxnet.io/data/cifar10/cifar10_val.rec -o cifar10_val.rec
 
 # MNIST
 MNIST="train-images-idx3-ubyte train-labels-idx1-ubyte t10k-images-idx3-ubyte t10k-labels-idx1-ubyte"
